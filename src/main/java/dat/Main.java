bdb0e906a0e2886575bee9b0913c7ce97266bf40
PejterNico <124774197+PejterNico@users.noreply.github.com>
2024-09-25 09:43:13 +0200

    First commit

---
+++ src/main/java/dat/Main.java
@@ -0,0 +1,13 @@
+package dat;
+
+import dat.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+    }
+}
